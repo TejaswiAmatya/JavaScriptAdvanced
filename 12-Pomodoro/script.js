ec2e9adcf1227ec9809d78c40c0adfe8a173b964
Tejaswi Amatya <tejuamatya@gmail.com>
2025-12-24 18:15:05 -0500

    Added pomodoro using setInterval

--- 12-Pomodoro/script.js
+++ 12-Pomodoro/script.js
@@ -1,4 +1,7 @@
 let toDoObject = [];
+let intervalId = null;
+const focusTime = 5;
+const breakTime = 2;
 function addTask() {
   const inputElement = document.querySelector(".inputTask");
   const name = inputElement.value;
@@ -31,7 +34,46 @@ function printTasks() {
 }
 
 function startTimer() {
-  timerId = setInterval(() => {}, 1000);
+  const statusElement = document.querySelector(".status");
+  const statusTimer = statusElement.innerHTML;
+
+  if (statusTimer === "Focus") {
+    startTime(focusTime);
+  } else {
+    startTime(breakTime);
+  }
+}
+
+function startTime(startTime) {
+  // Clear any existing interval
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+  }
+
+  let timeLeft = startTime;
+  intervalId = setInterval(() => {
+    timerElement = document.querySelector(".timer");
+    timeLeft--;
+    timerElement.innerHTML = formatTime(timeLeft);
+
+    // Stop when timer reaches 0
+    if (timeLeft < 0) {
+      clearInterval(intervalId);
+      intervalId = null;
+
+      const statusElement = document.querySelector(".status");
+      const currentStatus = statusElement.innerHTML;
+      const timerElement = document.querySelector(".timer");
+
+      if (currentStatus === "Focus") {
+        statusElement.innerHTML = "Break";
+        timerElement.innerHTML = formatTime(breakTime);
+      } else {
+        statusElement.innerHTML = "Focus";
+        timerElement.innerHTML = formatTime(focusTime);
+      }
+    }
+  }, 1000);
 }
 
 //this is to format time
